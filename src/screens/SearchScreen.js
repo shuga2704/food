d3409022ab6700478a62721f704ad79479d0b095
Dima Shuhayeu <shuga2704@gmail.com>
2022-03-13 14:09:17 +0300

    Init

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {View, Text, StyleSheet} from 'react-native'
+
+const SearchScreen = () => {
+    return <View>
+        <Text>Search screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default SearchScreen
