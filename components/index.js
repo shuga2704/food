29b1206e1c7202c7b9d0f30daf226c70c0dda8cd
Dima Shuhayeu <shuga2704@gmail.com>
2022-04-03 23:19:47 +0300

    UI is ready

--- components/index.js
+++ components/index.js
@@ -1,5 +1,18 @@
 import HomeHeader from './HomeHeader'
 import NFTCard from './NFTCard'
 import FocusedStatusBar from './FocusedStatusBar'
+import { CircleButton, RectButton } from './Button'
+import { SubInfo } from './SubInfo'
+import DetailsDesc from './DetailsDesc'
+import DetailsBid from './DetailsBid'
 
-export { HomeHeader, NFTCard, FocusedStatusBar }
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+}
